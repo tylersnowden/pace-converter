fceda8b370018a869cf84fe34bbcdfb84342b337
Tyler Snowden <tylersnowden@gmail.com>
2021-01-11 08:12:01 -0700

    Fix typo and change colors.

--- src/App.js
+++ src/App.js
@@ -82,15 +82,15 @@ class App extends Component {
 
           <div className="mt-1 flex rounded-md shadow-sm">
             <div className="relative flex items-stretch flex-grow focus-within:z-10">
-              <input type="text" name="pace" id="pace" className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300" placeholder="10:25" onChange={e => this.setState({pace: e.target.value})} />
+              <input type="text" name="pace" id="pace" className="focus:ring-blue-500 focus:border-blue-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300" placeholder="10:25" onChange={e => this.setState({pace: e.target.value})} />
               <div className="text-gray-600 absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
                 pace
               </div>
             </div>
-            <button className="-ml-px relative px-3 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 w-36 md:w-20" onClick={ this.convertKmToMi } >
+            <button className="-ml-px relative px-3 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 w-36 md:w-20" onClick={ this.convertKmToMi } >
               <span>to miles</span>
             </button>
-            <button className="-ml-px relative px-3 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 w-36 md:w-20" onClick={ this.convertMiToKm } >
+            <button className="-ml-px relative px-3 py-2 border border-gray-300 text-sm font-medium text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 rounded-r w-36 md:w-20" onClick={ this.convertMiToKm } >
               <span>to km</span>
             </button>
           </div>
